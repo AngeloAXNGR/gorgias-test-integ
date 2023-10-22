6e28061183ffa8ce6a05655d7c1b2fae84b68371
AngeloRSantos <angelo.s@chirpish.co>
2023-10-22 08:07:49 +0800

    get tickets

--- server.js
+++ server.js
@@ -29,25 +29,76 @@
 
 
 // https://gorgias-test-integ.onrender.com/api/customers/?email={{ticket.customer.email}}
+// This url should be the endpoints indicated in the API Reference Docs
+// const url = `https://gorgias-test-integ.onrender.com/api/customers?email=${email}`;
 
 const express = require('express');
 const app = express();
 const axios = require('axios');
+require('dotenv').config();
 
-app.get('/api/customers', async (req, res) => {
-  const email = req.query.email;
-	console.log(email);
-  const url = `https://gorgias-test-integ.onrender.com/api/customers?email=${email}`;
-  try {
-    const response = await axios.get(url);
-		console.log(response);
-    res.send(response.data);
-  } catch (error) {
+// app.get('/api/customers', async (req, res) => {
+//   const email = req.query.email;
+// 	console.log(email);
+//   console.log(process.env.GORGIAS_API)
+
+
+//   try {
+// 		const response = await axios
+// 		.request(options)
+// 		.then(function (response) {
+// 			// console.log(response.data);
+// 			res.send(response.data);
+// 		})
+// 		.catch(function (error) {
+// 			console.error(error);
+// 		});
+//   } catch (error) {
+//     // console.error(error);
+//     res.status(500).send('Internal server error');
+//   }
+// });
+
+app.get('/api/ticket/:id', async(req, res) => {
+  const id = req.params.id;
+  const options = {
+    method: 'GET',
+    url: `https://chirpish.gorgias.com/api/tickets/${id}`,
+    headers: {
+      accept: 'application/json',
+      authorization: process.env.GORGIAS_API
+    }
+  };
+
+  try{
+    const response = await axios
+      .request(options)
+      .then(function(response){
+        res.send(response.data)
+      })
+      .catch(function(error){
+        console.error(error)
+      })
+  }catch(error){
     console.error(error);
-    res.status(500).send('Internal server error');
+    res.status(500).send('Internal Server Error');
   }
-});
+})
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+// encode @ into %40 in email address
+// url: 'https://chirpish.gorgias.com/api/customers?email=angelo.s%40chirpish.co',
+
+// const options = {
+//   method: 'GET',
+// 	url: 'https://chirpish.gorgias.com/api/tickets/91320528',
+//   headers: {
+//     accept: 'application/json',
+// 		// BE CAREFUL WITH THIS
+//     authorization: process.env.GORGIAS_API
+//   }
+// };
+
+
+app.listen(4000, () => {
+  console.log('Server started on port 4000');
 });
